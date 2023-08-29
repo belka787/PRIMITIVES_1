4f9d5d568303cf56efb5f2d3627f39ab86722420
Marina Rodina <belka787@mail.ru>
2023-08-29 11:43:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 4_578;
+        int x = 20;
+
+        int bonus = ticketPrice / x;
+
+        System.out.println("Итоговый бонус " + bonus);
+
+
+    }
+}
